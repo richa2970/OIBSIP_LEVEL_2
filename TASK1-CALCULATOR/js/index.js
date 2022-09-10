83dd1b2ef7f6a1626ee3c79a728b998d6b9c3767
richa2970 <86349597+richa2970@users.noreply.github.com>
2022-09-10 12:15:57 +0530

    Add files via upload

---
+++ TASK1-CALCULATOR/js/index.js
@@ -0,0 +1,23 @@
+function insert(num) {
+  document.form.textview.value = document.form.textview.value + num;
+}
+
+function equal() {
+  var ere = document.form.textview.value;
+  if (ere) {
+    document.form.textview.value = eval(ere);
+  }
+}
+
+function c() {
+  document.form.textview.value = "";
+}
+
+function back() {
+  var ere = document.form.textview.value;
+  document.form.textview.value = ere.substring(0, ere.length - 1);
+}
+
+function sqrt() {
+  screen.value = Math.sqrt(screen.value, 2);
+}
